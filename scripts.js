aea0dd00f88a5b15ecb10ec98471393179c48e6d
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-04-12 12:26:01 -0300

    Funções do projeto otimizadas

--- scripts.js
+++ scripts.js
@@ -1,38 +1,42 @@
 // Declaração das variáveis globais
-
 let array = [];
 let soma = "";
+
+function ativarBorda(categoria, opção, botao) {
+
+     /* comandos de seleção da borda */
+
+     const desativaBotao = document.querySelector(`${categoria} .bordaverde`);
+     if (desativaBotao !== null) {
+         desativaBotao.classList.remove("bordaverde");
+     }
+     const ativaBotao = document.querySelector("." + botao);
+     ativaBotao.classList.add("bordaverde");
+ 
+     /* comandos de seleção do icone */
+ 
+     for (let i = 1; i < 5; i++) {
+         const desativaIcone = document.querySelector(`${categoria} ${opção}${i} .icone-check`);
+         desativaIcone.classList.add("escondido");
+         const ativaIcone = document.querySelector("." + botao + " .icone-check");
+         ativaIcone.classList.remove("escondido");
+     }
+}
 
 function selecionarComida(classeBotao) {
     let nomePrato = "";
     let precoPrato = "";
 
-    /* comandos de seleção da borda */
+    ativarBorda(".comidas",".comida", classeBotao);
 
-    const desativaBotao = document.querySelector(".comidas .bordaverde");
-    if (desativaBotao !== null) {
-        desativaBotao.classList.remove("bordaverde");
-    }
-    const ativaBotao = document.querySelector("." + classeBotao);
-    ativaBotao.classList.add("bordaverde");
-
-    /* comandos de seleção do icone */
-
-    for (let i = 1; i < 5; i++) {
-        const desativaIcone = document.querySelector(".comidas .comida" + i + " .icone-check");
-        desativaIcone.classList.add("escondido");
-        const ativaIcone = document.querySelector("." + classeBotao + " .icone-check");
-        ativaIcone.classList.remove("escondido");
-    }
-
-    //Pegar os dados do botão clicado que estão no html
+    /* Pegar os dados do botão clicado que estão no html */
 
     nomePrato = document.querySelector("." + classeBotao + " .prato")
     nomePrato = nomePrato.innerHTML;
     precoPrato = document.querySelector("." + classeBotao + " .preco")
     precoPrato = precoPrato.innerHTML;
 
-    //Colocar os dados obtidos nas variáveis globais
+    /* Colocar os dados obtidos nas variáveis globais */
 
     array[0] = nomePrato;
     array[1] = precoPrato;
@@ -44,18 +48,9 @@ function selecionarComida(classeBotao) {
 function selecionarBebida(classeBotao) {
     let nomeBebida = "";
     let precoBebida = "";
-    const desativaBotao = document.querySelector(".bebidas .bordaverde");
-    if (desativaBotao !== null) {
-        desativaBotao.classList.remove("bordaverde");
-    }
-    const ativaBotao = document.querySelector("." + classeBotao);
-    ativaBotao.classList.add("bordaverde");
-    for (let i = 1; i < 5; i++) {
-        const desativaIcone = document.querySelector(".bebidas .bebida" + i + " .icone-check");
-        desativaIcone.classList.add("escondido");
-        const ativaIcone = document.querySelector("." + classeBotao + " .icone-check");
-        ativaIcone.classList.remove("escondido");
-    }
+
+    ativarBorda(".bebidas",".bebida", classeBotao);
+
     nomeBebida = document.querySelector("." + classeBotao + " .prato")
     nomeBebida = nomeBebida.innerHTML;
     precoBebida = document.querySelector("." + classeBotao + " .preco")
@@ -70,18 +65,9 @@ function selecionarBebida(classeBotao) {
 function selecionarSobremesa(classeBotao) {
     let nomeSobremesa = "";
     let precoSobremesa = "";
-    const desativaBotao = document.querySelector(".sobremesas .bordaverde");
-    if (desativaBotao !== null) {
-        desativaBotao.classList.remove("bordaverde");
-    }
-    const ativaBotao = document.querySelector("." + classeBotao);
-    ativaBotao.classList.add("bordaverde");
-    for (let i = 1; i < 5; i++) {
-        const desativaIcone = document.querySelector(".sobremesas .sobremesa" + i + " .icone-check");
-        desativaIcone.classList.add("escondido");
-        const ativaIcone = document.querySelector("." + classeBotao + " .icone-check");
-        ativaIcone.classList.remove("escondido");
-    }
+
+    ativarBorda(".sobremesas",".sobremesa", classeBotao);
+    
     nomeSobremesa = document.querySelector("." + classeBotao + " .prato")
     nomeSobremesa = nomeSobremesa.innerHTML;
     precoSobremesa = document.querySelector("." + classeBotao + " .preco")
@@ -92,8 +78,6 @@ function selecionarSobremesa(classeBotao) {
     ativarBotao();
     return array;
 }
-
-// Essa função está dentro das funções acima
 
 function ativarBotao() {
     if (array[6] === "ok" && array[7] === "ok" && array[8] === "ok") {
@@ -106,18 +90,15 @@ function ativarBotao() {
     }
 }
 
-// A função deve remover o R$ do preço, trocar , por . para fazer a conta
+/* A função deve remover o R$ do preço, trocar , por . para fazer a conta */
 
 function calcularPreco() {
     let num1 = array[1];
-    num1 = num1.substr(3);
-    num1 = num1.replace(",", ".");
+    num1 = num1.substr(3).replace(",", ".");
     let num2 = array[3];
-    num2 = num2.substr(3);
-    num2 = num2.replace(",", ".");
+    num2 = num2.substr(3).replace(",", ".");
     let num3 = array[5];
-    num3 = num3.substr(3);
-    num3 = num3.replace(",", ".");
+    num3 = num3.substr(3).replace(",", ".");
     soma = parseFloat(num1) + parseFloat(num2) + parseFloat(num3);
     soma = soma.toFixed(2).replace(".", ",");
     return soma;
@@ -126,12 +107,12 @@ function calcularPreco() {
 function confirmarPedido() {
     calcularPreco();
 
-    // Comandos para mostrar a barra de confirmação
+    /* Comandos para mostrar a barra de confirmação */
 
     let pedido = document.querySelector(".confirmacao");
     pedido.classList.remove("escondido");
 
-    // Envia os valores dentro do JS para o HTML
+    /* Envia os valores dentro do JS para o HTML */
 
     document.getElementById("comida").innerHTML = array[0];
     document.getElementById("precocomida").innerHTML = array[1].substr(3);
@@ -142,17 +123,24 @@ function confirmarPedido() {
     document.getElementById("precototal").innerHTML = "R$ " + soma;
 }
 
-// Função que coloca a página sem nada selecionado
-
 function recomeçar() {
     let pedido = document.querySelector(".confirmacao");
     pedido.classList.add("escondido");
+    resetarBordas();
+    resetarIcones();
+    zerarBotao();
+}
+
+function resetarBordas() {
     let zerar1 = document.querySelector(".comidas .bordaverde");
     zerar1.classList.remove("bordaverde");
     let zerar2 = document.querySelector(".bebidas .bordaverde");
     zerar2.classList.remove("bordaverde");
     let zerar3 = document.querySelector(".sobremesas .bordaverde");
     zerar3.classList.remove("bordaverde");
+}
+
+function resetarIcones() {
     for (let i = 1; i < 5; i++) {
         const zerar4 = document.querySelector(".comidas .comida" + i + " .icone-check");
         zerar4.classList.add("escondido");
@@ -163,19 +151,21 @@ function recomeçar() {
     }
 }
 
+function zerarBotao() {
+    const desativar = document.querySelector(".desativado");
+    desativar.classList.remove("escondido");
+    const ativar = document.querySelector(".ativado");
+    ativar.classList.add("escondido");
+    array[6] = "";
+    array[7] = "";
+    array[8] = "";
+}
+
 function enviarMensagem() {
     const nomeUsurario = prompt("Escreva seu nome")
     const enderecoUsurario = prompt("Escreva seu endereço")
     calcularPreco();
-    textoMensagem = "Olá, gostaria de fazer o pedido: - Prato: " + array[0] + " - Bebida: " + array[2] + " - Sobremesa: " + array[4] + " Total: R$ " + soma + " Nome: " + nomeUsurario + " Endereço: " + enderecoUsurario;
+    textoMensagem = `Olá, gostaria de fazer o pedido: - Prato: ${array[0]} - Bebida: ${array[2]} 
+    - Sobremesa: ${array[4]} Total: R$ ${soma} Nome: ${nomeUsurario} Endereço: ${enderecoUsurario}`;
     window.open(`https://wa.me/5511972665730?text=${encodeURIComponent(textoMensagem)}`);
 }
-
-
-
-
-
-
-
-
-
